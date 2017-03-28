03b855871a2f07888685d547ff82baaa1d9fe3c8
ice1000 <ice1000kotlin@foxmail.com>
2017-03-29 00:22:57 +0800

    now you can create lice files

---
+++ src/lice/plugin/intellij/Utilities.kt
@@ -0,0 +1,29 @@
+package lice.plugin.intellij
+
+import com.intellij.ide.actions.CreateFileFromTemplateAction
+import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+
+/**
+ * Created by ice1000 on 2017/3/28.
+ *
+ * @author ice1000
+ */
+class NewLiceFile : CreateFileFromTemplateAction(CAPTION, "", LICE_ICON), DumbAware {
+	override fun buildDialog(
+			p0: Project?,
+			p1: PsiDirectory?,
+			builder: CreateFileFromTemplateDialog.Builder) {
+		builder
+				.setTitle(CAPTION)
+				.addKind("Empty File", LICE_ICON, "Lice File")
+	}
+
+	override fun getActionName(p0: PsiDirectory?, p1: String?, p2: String?) = CAPTION
+
+	private companion object Caption {
+		val CAPTION = "New Lice File"
+	}
+}
