163b1a8defe3c47cb26b93a71a7616c784daf322
ice1000 <ice1000kotlin@foxmail.com>
2017-03-30 01:13:10 +0800

    anyway, ah...

--- src/org/lice/lang/Editing.kt
+++ src/org/lice/lang/Editing.kt
@@ -6,20 +6,26 @@
 package org.lice.lang
 
 import com.intellij.CommonBundle
+import com.intellij.icons.AllIcons
 import com.intellij.ide.actions.CreateFileAction
 import com.intellij.lang.Commenter
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.io.FileUtilRt
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFileFactory
 import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy
 import com.intellij.spellchecker.tokenizer.Tokenizer
+import org.lice.Lice
 import org.lice.lang.LiceInfo.EXTENSION
 import org.lice.lang.LiceInfo.LICE_ICON
+import java.io.File
 
 
 class NewLiceFile : CreateFileAction(CAPTION, "", LICE_ICON) {
@@ -36,10 +42,10 @@ class NewLiceFile : CreateFileAction(CAPTION, "", LICE_ICON) {
 			name: String?,
 			directory: PsiDirectory?
 	): Array<PsiElement?> {
-		val origin = name ?: "new-file${System.currentTimeMillis()}.lice"
+		val origin = name ?: "new-file${System.currentTimeMillis()}.$EXTENSION"
 		val fixedExtension = when (FileUtilRt.getExtension(origin)) {
-			"lice" -> origin
-			else -> "$origin.lice"
+			EXTENSION -> origin
+			else -> "$origin.$EXTENSION"
 		}
 		return arrayOf(directory?.add(PsiFileFactory
 				.getInstance(directory.project)
@@ -56,7 +62,7 @@ class NewLiceFile : CreateFileAction(CAPTION, "", LICE_ICON) {
 				"Enter a new lice script name",
 				"New Lice script",
 				Messages.getQuestionIcon(),
-				"file-name${System.currentTimeMillis()}.lice",
+				"file-name${System.currentTimeMillis()}.$EXTENSION",
 				validator
 		)
 		return validator.createdElements
@@ -67,10 +73,27 @@ class NewLiceFile : CreateFileAction(CAPTION, "", LICE_ICON) {
 	}
 }
 
-class RunLiceFile : AnAction("Run Lice script", "Run Lice script", LiceInfo.LICE_BIG_ICON) {
-	override fun actionPerformed(p0: AnActionEvent?) {
-		// TODO
+class RunLiceFile : AnAction(
+		"Run Lice script",
+		"Run Lice script",
+		AllIcons.Toolwindows.ToolWindowRun) {
+	override fun actionPerformed(e: AnActionEvent) {
+		compatibleFiles(e).forEach { file ->
+			FileDocumentManager
+					.getInstance()
+					.getDocument(file)?.let { doc ->
+				FileDocumentManager
+						.getInstance()
+						.saveDocument(doc)
+			}
+			Lice.run(File(file.path))
+		}
 	}
+
+	private fun compatibleFiles(e: AnActionEvent): Array<out VirtualFile> =
+			(CommonDataKeys.VIRTUAL_FILE_ARRAY.getData(e.dataContext) ?: emptyArray()).filter { file ->
+				file is VirtualFile && EXTENSION == file.extension
+			}.toTypedArray()
 }
 
 class LiceCommenter : Commenter {
