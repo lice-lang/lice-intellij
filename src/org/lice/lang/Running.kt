3bafe56e9f43c0fabb676fc5018d433487724f55
ice1000 <ice1000kotlin@foxmail.com>
2017-04-01 01:32:06 +0800

    add swing gui components

--- src/org/lice/lang/Running.kt
+++ src/org/lice/lang/Running.kt
@@ -6,10 +6,16 @@
 package org.lice.lang
 
 import com.intellij.execution.Executor
+import com.intellij.execution.configuration.EnvironmentVariablesComponent
 import com.intellij.execution.configurations.*
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.ui.RawCommandLineEditor
+import com.intellij.ui.components.JBLabel
+import java.awt.GridBagConstraints
+import java.awt.Insets
 import javax.swing.JPanel
 
 object LiceConfigurationType : ConfigurationTypeBase(
@@ -23,6 +29,57 @@ object LiceConfigurationType : ConfigurationTypeBase(
 	}
 }
 
+class LiceRunConfigPanel(
+		private var scriptComponent: TextFieldWithBrowseButton,
+		private var programParametersComponent: RawCommandLineEditor,
+		private var workingDirectoryComponent: TextFieldWithBrowseButton,
+		private var environmentVariables: EnvironmentVariablesComponent) : JPanel() {
+
+	init {
+		val base: () -> GridBagConstraints = {
+			GridBagConstraints().apply {
+				insets = Insets(3, 3, 3, 3)
+				anchor = GridBagConstraints.LINE_START
+			}
+		}
+
+		val moduleLabel = JBLabel("Script Path")
+		add(moduleLabel, base().apply {
+			gridx = 0
+			gridy = 0
+			weightx = 0.1
+			fill = GridBagConstraints.HORIZONTAL
+		})
+
+		add(scriptComponent, base().apply {
+			gridx = 1
+			fill = GridBagConstraints.HORIZONTAL
+			weightx = 1.0
+		})
+
+		add(JBLabel("Working directory"), base().apply {
+			gridx = 0
+			gridy = 1
+			weightx = 0.1
+		})
+
+		add(workingDirectoryComponent, base().apply {
+			gridx = 1
+			gridy = 1
+			weightx = 0.1
+			fill = GridBagConstraints.HORIZONTAL
+		})
+
+		add(environmentVariables, base().apply {
+			gridx = 0
+			gridwidth = 2
+			gridy = 2
+			weightx = 0.1
+			fill = GridBagConstraints.HORIZONTAL
+		})
+	}
+}
+
 class LiceRunConfiguration(
 		project: Project,
 		factory: ConfigurationFactory
@@ -30,18 +87,34 @@ class LiceRunConfiguration(
 	override fun checkConfiguration() = Unit
 	override fun getConfigurationEditor(): SettingsEditor<out RunConfiguration> =
 			object : SettingsEditor<LiceRunConfiguration>() {
-				override fun createEditor() = JPanel()
+				private val panel = LiceRunConfigPanel(
+						TextFieldWithBrowseButton(),
+						RawCommandLineEditor(),
+						TextFieldWithBrowseButton(),
+						EnvironmentVariablesComponent()
+				)
+
+				override fun createEditor() = panel
 				override fun resetEditorFrom(configuration: LiceRunConfiguration) = Unit
 				override fun applyEditorTo(configuration: LiceRunConfiguration) = Unit
 			}
 
-	override fun getState(p0: Executor, p1: ExecutionEnvironment): RunProfileState? {
-		return null
+	override fun getState(
+			executor: Executor,
+			environment: ExecutionEnvironment
+	) = object : JavaCommandLineState(environment) {
+		override fun createJavaParameters() = JavaParameters().apply {
+			workingDirectory = project.basePath
+			jarPath = LiceInfo.LICE_PATH
+			classPath.add(LiceInfo.KOTLIN_RUNTIME_PATH)
+			classPath.add(LiceInfo.KOTLIN_REFLECT_PATH)
+			mainClass = "org.lice.repl.Main"
+			vmParametersList.add("D:/git-repos/lice-intellij/test.lice")
+		}
 	}
-
 }
 
 object LiceConfigurationFactory : ConfigurationFactory(LiceConfigurationType) {
-	override fun createTemplateConfiguration(p0: Project) = LiceRunConfiguration(p0, this)
+	override fun createTemplateConfiguration(project: Project) = LiceRunConfiguration(project, this)
 }
 
