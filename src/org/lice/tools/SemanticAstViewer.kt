3ec0b37d133b367a126c60e70aab4f40316142a5
ice1000 <ice1000kotlin@foxmail.com>
2017-03-31 20:35:07 +0800

    add error handler

--- src/org/lice/tools/SemanticAstViewer.kt
+++ src/org/lice/tools/SemanticAstViewer.kt
@@ -6,16 +6,18 @@
 
 package org.lice.tools
 
-import com.intellij.ide.ui.laf.darcula.DarculaLaf
 import com.intellij.ui.components.JBScrollPane
 import org.lice.compiler.model.*
 import org.lice.compiler.parse.buildNode
 import org.lice.compiler.parse.mapAst
-import org.lice.lang.LiceInfo
 import org.lice.repl.VERSION_CODE
 import java.awt.BorderLayout
+import java.awt.Dimension
 import java.io.File
-import javax.swing.*
+import javax.swing.JFrame
+import javax.swing.JTextArea
+import javax.swing.JTree
+import javax.swing.WindowConstants
 
 /**
  * map the ast
@@ -42,20 +44,24 @@ private fun createTreeRootFromFile(file: File): JTree {
  * @author ice1000
  */
 fun displaySemanticTree(file: File) {
-	UIManager.setLookAndFeel(DarculaLaf())
-	UIManager.put("Tree.collapsedIcon", LiceInfo.LICE_AST_NODE0_ICON)
-	UIManager.put("Tree.expandedIcon", LiceInfo.LICE_AST_NODE_ICON)
-	UIManager.put("Tree.openIcon", LiceInfo.LICE_AST_NODE0_ICON)
-	UIManager.put("Tree.closedIcon", LiceInfo.LICE_AST_NODE2_ICON)
-	UIManager.put("Tree.leafIcon", LiceInfo.LICE_AST_LEAF_ICON)
 	val frame = JFrame("Lice Semantic Tree $VERSION_CODE")
 	frame.layout = BorderLayout()
 	frame.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
 	frame.setLocation(80, 80)
-	frame.setSize(480, 480)
-	frame.add(
-			JBScrollPane(createTreeRootFromFile(file)),
-			BorderLayout.CENTER
-	)
+	try {
+		frame.add(
+				JBScrollPane(createTreeRootFromFile(file).apply {
+					preferredSize = Dimension(480, 480)
+				}),
+				BorderLayout.CENTER
+		)
+	} catch (e: RuntimeException) {
+		val label = JTextArea(e.message)
+		e.stackTrace.forEach { label.append("\t$it\n") }
+		label.isEditable = false
+		label.preferredSize = Dimension(600, 600)
+		frame.add(label, BorderLayout.CENTER)
+	}
+	frame.pack()
 	frame.isVisible = true
 }
