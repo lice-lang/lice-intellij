e2ae9a3bc1bbe5dda5abb94dba6cac00999a57b2
ice1000 <ice1000kotlin@foxmail.com>
2017-12-29 16:03:11 +0800

    why

--- src/org/lice/lang/lice-tokens.kt
+++ src/org/lice/lang/lice-tokens.kt
@@ -8,10 +8,10 @@ class LiceTokenType(debugName: String) : IElementType(debugName, LiceLanguage) {
 	companion object {
 		@JvmField val WHITE_SPACE: IElementType = TokenType.WHITE_SPACE
 		@JvmField val COMMENT = LiceTokenType("COMMENT")
-		@JvmField val LB = LiceTokenType("LEFT_BRACKET")
-		@JvmField val RB = LiceTokenType("RIGHT_BRACKET")
-		@JvmField val SYM = LiceTokenType("SYMBOL")
-		@JvmField val STR = LiceTokenType("STRING")
+		@JvmField val LB = LiceTokenType("LB")
+		@JvmField val RB = LiceTokenType("RB")
+		@JvmField val SYM = LiceTokenType("SYM")
+		@JvmField val STR = LiceTokenType("STR")
 		@JvmField val COMMENTS = TokenSet.create(COMMENT)
 		@JvmField val WHITE_SPACES = TokenSet.create(WHITE_SPACE)
 		@JvmField val STRINGS = TokenSet.create(STR)
