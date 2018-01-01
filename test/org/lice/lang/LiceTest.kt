2049038e54d32bae7f7ddedfbef5d32d85c74514
ice1000 <ice1000kotlin@foxmail.com>
2018-01-01 09:53:15 +0800

    [ stub ] Done generating stub.

--- test/org/lice/lang/LiceTest.kt
+++ test/org/lice/lang/LiceTest.kt
@@ -2,11 +2,30 @@ package org.lice.lang
 
 import org.junit.Test
 import org.lice.Lice
+import org.lice.core.SymbolList
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class StarPlatinum {
 	@Test
 	fun oraOraOraOra() {
 		//language=Lice
 		Lice.run("((233))").let(::println)
+		Lice.run("((\"666\"))").let(::println)
+	}
+
+	@Test
+	fun generateStdlibStub() {
+		(SymbolList.preludeSymbols.map { "(defexpr $it ()) ;; this is a stub, used for indexing" }
+				+
+				SymbolList.preludeVariables.map { "(-> $it ()) ;; this is a stub, used for indexing" })
+				.sorted()
+				.joinToString("\n")
+				.let {
+					println(it)
+					val destination = Paths.get("./lice-stdlib.lice")
+					if (!Files.exists(destination)) Files.createFile(destination)
+					Files.write(destination, it.toByteArray())
+				}
 	}
 }
