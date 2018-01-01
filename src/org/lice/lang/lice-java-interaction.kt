d83465322f99e0dfe16eb028e1caa9dd6644cc20
ice1000 <ice1000kotlin@foxmail.com>
2018-01-01 11:17:32 +0800

    [ #13 ] Remove validation.

--- src/org/lice/lang/lice-java-interaction.kt
+++ src/org/lice/lang/lice-java-interaction.kt
@@ -13,23 +13,19 @@ class LiceSymbolsExtractingAnnotator : Annotator {
 		val calleeType = callee.type ?: return
 		if (calleeType.isValid && calleeType.canonicalText == SymbolList::class.java.canonicalName) {
 			val method = methodCall.methodExpression.children.first { it is PsiIdentifier }
-			// SymbolList().defineFunction()
-			// SymbolList().provideFunctionWithMeta()
-			// SymbolList().provideFunction()
-			if (method.isValid) {
-				if ("Function" in method.text) {
-					val possibleString = methodCall.argumentList.children
-							.first { it is PsiLiteralExpression } as? PsiLiteralExpression ?: return
-					if (possibleString.value is String) holder.createInfoAnnotation(
-							TextRange(possibleString.textRange.startOffset + 1, possibleString.textRange.endOffset - 1), null)
-							.textAttributes = LiceSyntaxHighlighter.FUNCTION_DEFINITION
-				} else if ("Variable" in method.text) {
-					val possibleString = methodCall.argumentList.children
-							.first { it is PsiLiteralExpression } as? PsiLiteralExpression ?: return
-					if (possibleString.value is String) holder.createInfoAnnotation(
-							TextRange(possibleString.textRange.startOffset + 1, possibleString.textRange.endOffset - 1), null)
-							.textAttributes = LiceSyntaxHighlighter.VARIABLE_DEFINITION
-				}
+			// defineFunction provideFunctionWithMeta provideFunction
+			if ("Function" in method.text) {
+				val possibleString = methodCall.argumentList.children
+						.first { it is PsiLiteralExpression } as? PsiLiteralExpression ?: return
+				if (possibleString.value is String) holder.createInfoAnnotation(
+						TextRange(possibleString.textRange.startOffset + 1, possibleString.textRange.endOffset - 1), "Lice function")
+						.textAttributes = LiceSyntaxHighlighter.FUNCTION_DEFINITION
+			} else if ("Variable" in method.text) {
+				val possibleString = methodCall.argumentList.children
+						.first { it is PsiLiteralExpression } as? PsiLiteralExpression ?: return
+				if (possibleString.value is String) holder.createInfoAnnotation(
+						TextRange(possibleString.textRange.startOffset + 1, possibleString.textRange.endOffset - 1), "Lice variable")
+						.textAttributes = LiceSyntaxHighlighter.VARIABLE_DEFINITION
 			}
 		}
 	}
