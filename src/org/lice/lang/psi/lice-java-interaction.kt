3662dac50815d95bbdc33d2647d6c797f1e85f20
ice1000 <ice1000kotlin@foxmail.com>
2018-01-02 15:42:25 +0800

    [ info ] Update plugin description, fix bug.

--- src/org/lice/lang/psi/lice-java-interaction.kt
+++ src/org/lice/lang/psi/lice-java-interaction.kt
@@ -10,20 +10,20 @@ import org.lice.lang.LiceSyntaxHighlighter
 class LiceSymbolsExtractingAnnotator : Annotator {
 	override fun annotate(element: PsiElement, holder: AnnotationHolder) {
 		val methodCall = element as? PsiMethodCallExpression ?: return
-		val callee = methodCall.methodExpression.children.first { it is PsiExpression } as? PsiExpression ?: return
+		val callee = methodCall.methodExpression.children.firstOrNull { it is PsiExpression } as? PsiExpression ?: return
 		val calleeType = callee.type ?: return
 		if (calleeType.isValid && calleeType.canonicalText == SymbolList::class.java.canonicalName) {
-			val method = methodCall.methodExpression.children.first { it is PsiIdentifier }
+			val method = methodCall.methodExpression.children.firstOrNull { it is PsiIdentifier } ?: return
 			// defineFunction provideFunctionWithMeta provideFunction
 			if ("Function" in method.text) {
 				val possibleString = methodCall.argumentList.children
-						.first { it is PsiLiteralExpression } as? PsiLiteralExpression ?: return
+						.firstOrNull { it is PsiLiteralExpression } as? PsiLiteralExpression ?: return
 				if (possibleString.value is String) holder.createInfoAnnotation(
 						TextRange(possibleString.textRange.startOffset + 1, possibleString.textRange.endOffset - 1), "Lice function")
 						.textAttributes = LiceSyntaxHighlighter.FUNCTION_DEFINITION
 			} else if ("Variable" in method.text) {
 				val possibleString = methodCall.argumentList.children
-						.first { it is PsiLiteralExpression } as? PsiLiteralExpression ?: return
+						.firstOrNull { it is PsiLiteralExpression } as? PsiLiteralExpression ?: return
 				if (possibleString.value is String) holder.createInfoAnnotation(
 						TextRange(possibleString.textRange.startOffset + 1, possibleString.textRange.endOffset - 1), "Lice variable")
 						.textAttributes = LiceSyntaxHighlighter.VARIABLE_DEFINITION
