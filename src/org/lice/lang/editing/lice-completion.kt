99fa3c992967a1d0109d6394b1e5c889c3715d1c
ice1000 <ice1000kotlin@foxmail.com>
2018-01-02 14:39:53 +0800

    [ #11 ] Add stdlib based completion.

---
+++ src/org/lice/lang/editing/lice-completion.kt
@@ -0,0 +1,17 @@
+package org.lice.lang.editing
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.util.ProcessingContext
+import org.lice.lang.psi.LiceTypes
+
+class LiceBuiltinSymbolsCompletionContributor : CompletionContributor() {
+	init {
+		extend(CompletionType.BASIC, PlatformPatterns.psiElement(LiceTypes.SYM), object : CompletionProvider<CompletionParameters>() {
+			override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext?, resultSet: CompletionResultSet) {
+				resultSet.addAllElements(LiceSymbols.allSymbols.map(LookupElementBuilder::create))
+			}
+		})
+	}
+}
