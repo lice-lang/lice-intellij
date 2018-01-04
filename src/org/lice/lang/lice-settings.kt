55ae9f7245f0b6f4ec0c02a67c85d0e1fa407de4
ice1000 <ice1000kotlin@foxmail.com>
2018-01-05 00:23:19 +0800

    [ color ] Fix color

--- src/org/lice/lang/lice-settings.kt
+++ src/org/lice/lang/lice-settings.kt
@@ -3,13 +3,15 @@ package org.lice.lang
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.*
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.components.PathMacroManager
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.options.colors.*
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.JDOMExternalizer
 import com.intellij.ui.layout.panel
 import org.intellij.lang.annotations.Language
-import java.awt.TextField
-import javax.swing.JComponent
+import org.jdom.Element
 
 class LiceColorSettingsPage : ColorSettingsPage {
 	private companion object {
@@ -54,25 +56,58 @@ class LiceConfigurationType : ConfigurationType {
 	override fun getConfigurationFactories() = arrayOf(LiceConfigurationFactory(this))
 }
 
-class LiceConfigurationFactory(type: ConfigurationType) : ConfigurationFactory(type) {
+class LiceConfigurationFactory(
+		type: ConfigurationType) : ConfigurationFactory(type) {
 	override fun getName() = "Lice configuration factory"
 	override fun createTemplateConfiguration(project: Project) = LiceRunConfiguration(project, this)
 }
 
-class LiceRunConfiguration(project: Project, factory: ConfigurationFactory)
+class LiceRunConfiguration(
+		project: Project,
+		factory: ConfigurationFactory)
 	: RunConfigurationBase(project, factory, LICE_NAME) {
-	override fun getConfigurationEditor() = LiceSettingsEditor()
+	@JvmField var vmParams = ""
+	@JvmField var jarLocation = ""
+
+	override fun getConfigurationEditor() = LiceSettingsEditor(this, project)
 	override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState? = null
+	override fun writeExternal(element: Element) {
+		PathMacroManager.getInstance(project).expandPaths(element)
+		super.writeExternal(element)
+		JDOMExternalizer.write(element, "vmParams", vmParams)
+		JDOMExternalizer.write(element, "jarLocation", jarLocation)
+	}
+
+	override fun readExternal(element: Element) {
+		super.readExternal(element)
+		vmParams = JDOMExternalizer.readString(element, "vmParams") ?: ""
+		jarLocation = JDOMExternalizer.readString(element, "jarLocation") ?: ""
+		PathMacroManager.getInstance(project).collapsePathsRecursively(element)
+	}
 }
 
-class LiceSettingsEditor : SettingsEditor<LiceRunConfiguration>() {
-	override fun createEditor() = panel {
-		noteRow("Do something")
+class LiceSettingsEditor(
+		@JvmField internal var settings: LiceRunConfiguration,
+		@JvmField val project: Project) : SettingsEditor<LiceRunConfiguration>() {
+	override fun createEditor() = panel(title = "Lice Run Configuration") {
+		row("Lice jar path") {
+			textFieldWithBrowseButton("Select Lice jar",
+					fileChooserDescriptor = FileChooserDescriptor(false, false, true, false, false, false),
+					project = project,
+					fileChoosen = {
+						settings.jarLocation = it.path
+						settings.jarLocation
+					})
+		}
 	}
 
 	override fun applyEditorTo(configuration: LiceRunConfiguration) {
+		configuration.vmParams = settings.vmParams
+		configuration.jarLocation = settings.jarLocation
 	}
 
 	override fun resetEditorFrom(configuration: LiceRunConfiguration) {
+		settings.vmParams = configuration.vmParams
+		settings.jarLocation = configuration.jarLocation
 	}
 }
