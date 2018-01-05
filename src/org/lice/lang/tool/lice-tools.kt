0d1c940b05a58694475751b774d3fb7aae75cb24
ice1000 <ice1000kotlin@foxmail.com>
2018-01-05 14:32:35 +0800

    [ move ] Move files

--- src/org/lice/lang/tool/lice-tools.kt
+++ src/org/lice/lang/tool/lice-tools.kt
@@ -1,19 +1,46 @@
-/**
- * Created by ice1000 on 2017/3/30.
- *
- * @author ice1000
- */
-package org.lice.lang.tools
+package org.lice.lang.tool
 
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.treeStructure.Tree
 import org.apache.commons.lang.StringUtils
 import org.lice.model.*
 import org.lice.parse.buildNode
+import org.lice.parse.mapAst
+import java.awt.Dimension
 import java.io.File
 import javax.swing.JComponent
 import javax.swing.JTextArea
 import javax.swing.tree.DefaultMutableTreeNode
+
+object LiceSemanticTreeViewerFactory {
+	/**
+	 * map the ast
+	 */
+	private fun mapAst2Display(
+			node: Node,
+			viewRoot: DefaultMutableTreeNode
+	): DefaultMutableTreeNode = when (node) {
+		is ValueNode,
+		is SymbolNode,
+		is LazyValueNode -> DefaultMutableTreeNode(node)
+		is ExpressionNode -> viewRoot.apply {
+			add(mapAst2Display(node.node, DefaultMutableTreeNode(node.node)))
+			node.params.forEach { add(mapAst2Display(it, DefaultMutableTreeNode(it))) }
+		}
+		else -> DefaultMutableTreeNode("unknown node")
+	}
+
+	private fun createTreeRootFromFile(file: File) =
+			mapAst(buildNode(file.readText())).let { ast -> Tree(mapAst2Display(ast, DefaultMutableTreeNode(ast))) }
+
+	fun create(file: File): JComponent = try {
+		JBScrollPane(createTreeRootFromFile(file).apply {
+			preferredSize = Dimension(520, 520)
+		})
+	} catch (e: Exception) {
+		JTextArea(e.message)
+	}
+}
 
 object LiceSyntaxTreeViewerFactory {
 	/**
@@ -56,6 +83,7 @@ object LiceSyntaxTreeViewerFactory {
 		JTextArea(e.message)
 	}
 }
+
 /*
 /**
  * @author ice1000
